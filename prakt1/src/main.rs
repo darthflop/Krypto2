805e572ee6df97f85bd0ee0737c069d6ac8c834f
darthflop <filip3016@googlemail.com>
2025-04-16 22:13:28 +0200

    added message signing

--- prakt1/src/main.rs
+++ prakt1/src/main.rs
@@ -5,7 +5,7 @@ use num_prime::RandPrime;
 use rand::thread_rng;
 
 
-struct Keypair {
+struct KeyPair {
     public_key: PublicKey,
     private_key: PrivateKey
 }
@@ -23,18 +23,33 @@ struct PrivateKey {
 
 fn main() {
     
+    // generate rsa keypair
     let keypair = keygen();
 
-    // print keypair values
-    println!("n = {}", keypair.public_key.n);
-    println!("p = {}", keypair.private_key.p);
-    println!("q = {}", keypair.private_key.q);
-    println!("e = {}", keypair.public_key.e);
-    println!("d = {}", keypair.private_key.d);
+    // declare message
+    let message: u32 = 4;
+    let x: BigUint = BigUint::from(message);
+
+    // sign message
+    let s = sign(x, &keypair);
+
+    // print signed message
+    println!("Signed message:\n {}\n\n", s);
+
+    // print rsa key values
+    print_key_values(&keypair);
+
 }
 
+
+// signs a message by calculating m^d % n 
+fn sign(m: BigUint, key_pair: &KeyPair) -> BigUint {
+    return m.modpow(&key_pair.private_key.d, &key_pair.public_key.n);
+}
+
+
 // generates rsa keypair
-fn keygen() -> Keypair {
+fn keygen() -> KeyPair {
 
     let mut rng = thread_rng();
 
@@ -52,11 +67,19 @@ fn keygen() -> Keypair {
 
     let d = e.modinv(&phi).expect("Modularer Inverser konnte nicht berechnet werden");
 
-    return Keypair {
+    return KeyPair {
         public_key: PublicKey { n, e },
         private_key: PrivateKey {p, q, d},
     };
 }
 
 
-
+fn print_key_values(keypair: &KeyPair) {
+    println!("--------------------------------------------------------------------------\n\n");
+    println!("Generated keypair values:\n");
+    println!("n = {}\n", keypair.public_key.n);
+    println!("p = {}\n", keypair.private_key.p);
+    println!("q = {}\n", keypair.private_key.q);
+    println!("e = {}\n", keypair.public_key.e);
+    println!("d = {}\n", keypair.private_key.d);
+}
